a5967565ebe633d711e04ce1fca436ed57f4940a
Connor <connoroakvillea32@hotmail.com>
2023-10-04 17:32:14 -0400

    finished accordian

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
+import AccordianList from './AccordianList';
 
 function About() {
   return(
     <div className="flex flex-col justify-center items-center min-h-[90vh]">
-      <h1>accordian for coursework?</h1>
       <div className="w-[700px] space-y-4">
         <div>
           <div className="mb-6">
@@ -21,54 +21,9 @@ function About() {
               The dynamic nature of technology, from software engineering to machine learning and data analysis, 
               keeps me motivated to expand my skill set and embrace new challenges.
             </p>
-            <p>
-              Below is some of my relevant coursework from school...
-            </p>
           </div>
         </div>
-        <div className="space-y-2">
-          <h6>
-            Software Dev
-            <span>
-              : Software Engineering,
-              Data Structures II,
-              Algorithm Design & Analysis,
-              Object-Oriented Programming,
-              Operating Systems,
-              Computer Security
-            </span>
-          </h6>
-          <h6>
-            Data & Analysis
-            <span>
-              : Machine Learning,
-              Artificial Intelligence,
-              Databases,
-              Managerial Statistics,
-              Windows Application Programming
-            </span>
-          </h6>
-          <h6>
-            Math
-            <span>
-              : Calculus I,
-              Linear Algebra,
-              Mathematical Proofs,
-              Discrete Mathematics
-            </span>
-          </h6>
-          <h6>
-            Finance
-            <span>
-              : Financial Management II,
-              Options, Futures & Swaps,
-              Fixed Income Analysis,
-              Advanced Corporate Finance,
-              Investment Management
-            </span>
-          </h6>
-          <p></p>
-        </div>
+        <AccordianList/>
       </div>
     </div>
   );
---
+++ src/components/AccordianList.jsx
@@ -0,0 +1,38 @@
+import Accordian from './Accordian';
+
+function AccordianList() {
+  return (
+    <div class="hs-accordion-group">
+      <Accordian 
+        title={"Software Dev Courses"}
+        content={"Software Engineering, Data Structures II, Algorithm Design & Analysis, Object-Oriented Programming, Operating Systems, Computer Security"} 
+        active={" active"} 
+        hidden={""} 
+        number={"one"}
+      />
+      <Accordian 
+        title={"Data & Analysis Courses"} 
+        content={"Machine Learning, Artificial Intelligence, Databases, Managerial Statistics, Windows Application Programming"}
+        active={""} 
+        hidden={" hidden"} 
+        number={"two"}
+      />
+      <Accordian 
+        title={"Math Courses"}
+        content={"Calculus I, Linear Algebra, Mathematical Proofs, Discrete Mathematics"} 
+        active={""} 
+        hidden={" hidden"} 
+        number={"three"}
+      />
+      <Accordian 
+        title={"Finance Courses"} 
+        content={"inancial Management II, Options, Futures & Swaps, Fixed Income Analysis, Advanced Corporate Finance, Investment Management"}
+        active={""} 
+        hidden={" hidden"} 
+        number={"four"}
+      />
+    </div>
+  );
+}
+
+export default AccordianList;
