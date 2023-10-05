2d20fdb5862c57e716b7806e87ba04ad99258bba
Connor <connoroakvillea32@hotmail.com>
2023-10-05 11:12:44 -0400

    initial work on making card elements take a dynamic amount of content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,43 @@
 
-function Card({title, subtitle, content, link}) {
-  console.log(content);
+function Card({title, subtitles, contents, link}) {
 
-  // const contentArr = content.map(section => {
-  //   <p className="mt-2 text-gray-800 dark:text-gray-400">
-  //     {section}
-  //   </p>
-  // });
-  return (
-    <div className="flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-      <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-        {title}
-      </h3>
-      <p className="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
+  const subtitleArr = subtitles.map((subtitle, i) => {
+    return (
+      <p key={i} className="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
         {subtitle}
       </p>
-      <p className="mt-2 text-gray-800 dark:text-gray-400">
+    );
+  });
+
+  const contentArr = contents.map((content, i) => {
+    return (
+      <p key={i} className="mt-2 dark:text-gray-400">
         {content}
       </p>
+    );
+  });
+
+  const sectionArr = [];
+
+  for(let i = 0; i < contentArr.length; i++) {
+    sectionArr.push(subtitleArr[i]);
+    sectionArr.push(contentArr[i]);
+  }
+
+  return (
+    <div className="flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+      <h5>
+        {title}
+      </h5>
+      <div>
+        {sectionArr}
+      </div>
+      <p className="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
+        {subtitles}
+      </p>
+      <div className="">
+        {contentArr}
+      </div>
       <a className="mt-3 inline-flex items-center gap-2 mt-5 text-sm font-medium text-blue-500 hover:text-blue-700" href={link}>
         Git Repo
         <svg className="w-2.5 h-auto" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
