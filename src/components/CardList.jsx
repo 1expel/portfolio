784f423d224fe9407716cd655d36f0c7e5d09c50
Connor <connoroakvillea32@hotmail.com>
2023-10-05 10:17:45 -0400

    connected cards to tabs

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,37 +1,32 @@
+import Card from './Card';
 
-function CardList() {
+function CardList({tab1, card1, tab2, card2, tab3, card3}) {
   return (
     <div>
-      
+
       <div class="border-b border-gray-200 dark:border-gray-700">
         <nav class="flex space-x-2" aria-label="Tabs" role="tablist">
           <button type="button" class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 active" id="tabs-with-underline-item-1" data-hs-tab="#tabs-with-underline-1" aria-controls="tabs-with-underline-1" role="tab">
-            Tab 1
+            {tab1}
           </button>
           <button type="button" class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600" id="tabs-with-underline-item-2" data-hs-tab="#tabs-with-underline-2" aria-controls="tabs-with-underline-2" role="tab">
-            Tab 2
+            {tab2}
           </button>
           <button type="button" class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600" id="tabs-with-underline-item-3" data-hs-tab="#tabs-with-underline-3" aria-controls="tabs-with-underline-3" role="tab">
-            Tab 3
+            {tab3}
           </button>
         </nav>
       </div>
 
       <div class="mt-3">
         <div id="tabs-with-underline-1" role="tabpanel" aria-labelledby="tabs-with-underline-item-1">
-          <p class="text-gray-500 dark:text-gray-400">
-            This is the <em class="font-semibold text-gray-800 dark:text-gray-200">first</em> item's tab body.
-          </p>
+          {card1}
         </div>
         <div id="tabs-with-underline-2" class="hidden" role="tabpanel" aria-labelledby="tabs-with-underline-item-2">
-          <p class="text-gray-500 dark:text-gray-400">
-            This is the <em class="font-semibold text-gray-800 dark:text-gray-200">second</em> item's tab body.
-          </p>
+          {card2}
         </div>
         <div id="tabs-with-underline-3" class="hidden" role="tabpanel" aria-labelledby="tabs-with-underline-item-3">
-          <p class="text-gray-500 dark:text-gray-400">
-            This is the <em class="font-semibold text-gray-800 dark:text-gray-200">third</em> item's tab body.
-          </p>
+          {card3}
         </div>
       </div>
 
