94ac27c1db7d75be4a9fe678bf7da65ad4524119
Connor <connoroakvillea32@hotmail.com>
2023-10-05 14:09:00 -0400

    adding svg components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from './components/Nav';
 import Landing from './components/Landing';
 import About from './components/About';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Landing/>
       <About/>
       <Projects/>
+      <Skills/>
     </div>
   );
 }
---
+++ src/assets/Sql.jsx
@@ -0,0 +1,6 @@
+
+export function Sql(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M14 3v4a1 1 0 0 0 1 1h4"></path><path d="M14 3v4a1 1 0 0 0 1 1h4M5 20.25c0 .414.336.75.75.75H7a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1H6a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1h1.25a.75.75 0 0 1 .75.75"></path><path d="M5 12V5a2 2 0 0 1 2-2h7l5 5v4m-1 3v6h2m-7-6a2 2 0 0 1 2 2v2a2 2 0 1 1-4 0v-2a2 2 0 0 1 2-2zm1 5l1.5 1.5"></path></g></svg>
+  )
+}
