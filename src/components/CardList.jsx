111f25f9fb17362d8057d09722b12c7ac252b5d6
Connor <connoroakvillea32@hotmail.com>
2023-10-07 14:29:30 -0400

    might have basically finished for the first deployment

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,32 +1,40 @@
 
-function CardList({children, tab1, tab2, tab3}) {
+function CardList({children, numbers, tab1, tab2, tab3}) {
   return (
     <div>
 
+      {/* PROLL CAN FIX BY JUST PASSING IN A NEW NUMBER FOR ID. IF THEY WRITE THEIR JS RIGHT IT SHHOULD WORKLOL */}
+
       <div className="border-b border-gray-200 dark:border-gray-700">
         <nav className="flex space-x-2" aria-label="Tabs" role="tablist">
-          <button type="button" className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm font-medium whitespace-nowrap text-gray-600 hover:text-blue-600 active" id="tabs-with-underline-item-1" data-hs-tab="#tabs-with-underline-1" aria-controls="tabs-with-underline-1" role="tab">
+          <button type="button" className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm font-medium whitespace-nowrap text-gray-600 hover:text-blue-600 active" id={"tabs-with-underline-item-" + numbers[0]} data-hs-tab={"#tabs-with-underline-" + numbers[0]} aria-controls={"tabs-with-underline-" + numbers[0]} role="tab">
             {tab1}
           </button>
-          <button type="button" className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm font-medium whitespace-nowrap text-gray-600 hover:text-blue-600" id="tabs-with-underline-item-2" data-hs-tab="#tabs-with-underline-2" aria-controls="tabs-with-underline-2" role="tab">
+          <button type="button" className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm font-medium whitespace-nowrap text-gray-600 hover:text-blue-600" id={"tabs-with-underline-item-" + numbers[1]} data-hs-tab={"#tabs-with-underline-" + numbers[1]} aria-controls={"tabs-with-underline-" + numbers[1]} role="tab">
             {tab2}
           </button>
+          {/* 
+          DO NOT DELETE SO YOU CAN REFERENCE CLASS NAMES WHEN YOU NEED A THIRD CARD
           <button type="button" className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-2 border-b-[3px] border-transparent text-sm font-medium whitespace-nowrap text-gray-600 hover:text-blue-600" id="tabs-with-underline-item-3" data-hs-tab="#tabs-with-underline-3" aria-controls="tabs-with-underline-3" role="tab">
             {tab3}
-          </button>
+          </button> 
+          */}
         </nav>
       </div>
 
       <div className="mt-3">
-        <div id="tabs-with-underline-1" role="tabpanel" aria-labelledby="tabs-with-underline-item-1">
+        <div id={"tabs-with-underline-" + numbers[0]} role="tabpanel" aria-labelledby={"tabs-with-underline-item-" + numbers[0]}>
           {children[0]}
         </div>
-        <div id="tabs-with-underline-2" className="hidden" role="tabpanel" aria-labelledby="tabs-with-underline-item-2">
+        <div id={"tabs-with-underline-" + numbers[1]} className="hidden" role="tabpanel" aria-labelledby={"tabs-with-underline-item-" + numbers[1]}>
           {children[1]}
         </div>
+        {/* 
+        DO NOT DELETE SO YOU CAN REFERENCE CLASS NAMES WHEN YOU NEED A THIRD CARD
         <div id="tabs-with-underline-3" className="hidden" role="tabpanel" aria-labelledby="tabs-with-underline-item-3">
           {children[2]}
-        </div>
+        </div> 
+        */}
       </div>
 
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ function Projects({projectsRef}) {
             Projects
           </h3>
         </div>
-        <CardList tab1={"Overview"} tab2={"Implementation"} tab3={"Tab 3"}>
+        <CardList numbers={[1,2,3]} tab1={"Overview"} tab2={"Implementation"} tab3={"Tab 3"}>
           <Card 
             title={"Hyperthyroidism Diagnosis Assistant"}
             subtitles={[
@@ -24,14 +24,29 @@ function Projects({projectsRef}) {
               This project uses machine-learning models to predict whether a patient has a hyperthyroid condition.`],
               [`The project uses a dataset from the Thyroid Disease Database from UCI machine learning repository which has more than 3000 cases. 
               The dataset has 22 variables which are medical characteristics of the patient that a machine learning model can use to assist human diagnosis.`],
-              [`insert results`]
+              [`The results of the model showed an 80% precision* in detecting patients with hyperthyroidism 
+              and an overall model accuracy* of 98%.`,
+              `note: precision = correct # of positive predictions / total # of positive predictions`,
+              `note: accuracy = correct # of predictions / total # of predictions`]
             ]}
             link={"https://github.com/ConkCodes/hyperthyroidism-predictor"}
           />
           <Card 
             title={"Hyperthyroidism Diagnosis Assistant"}
-            subtitles={["My Role/Tech Used"]}
-            contents={[["content"]]}
+            subtitles={[
+              "Tech Used",
+              "My Work"
+            ]}
+            contents={[
+              ["Language: Python", 
+              "Data Manipulation: Numpy, Pandas", 
+              "Machine Learning Models: Scikit-Learn", 
+              "Data Visualization: Matplotlib, Seaborn"],
+              [`Utilized Python libraries, Pandas and NumPy, to manipulate a dataset of 66,000+ data points.`,
+              `Used machine learning models from Scikit-Learn to diagnosis over 1000 patients in seconds.`,
+              `Achieved a precision score of 80% and overall model accuracy of 98%.`,
+              `Used libraries Matplotlib and Seaborn for data visualization, enhancing crucial insights.`]
+            ]}
             link={"https://github.com/ConkCodes/hyperthyroidism-predictor"}
           />
           <Card 
@@ -41,7 +56,7 @@ function Projects({projectsRef}) {
             link={"https://github.com/ConkCodes/hyperthyroidism-predictor"}
           />
         </CardList>
-        <CardList tab1={"Overview"} tab2={"Implementation"} tab3={"Tab 3"}>
+        <CardList numbers={[3,4,5]} tab1={"Overview"} tab2={"Implementation"} tab3={"Tab 3"}>
           <Card 
             title={"Flick - Your Ultimate Movie Night Decision-Maker!"}
             subtitles={[
@@ -52,12 +67,12 @@ function Projects({projectsRef}) {
               [`Flick is a user-friendly web application designed to enhance the movie-watching experience for groups of friends. 
               No more endless debates and scrolling through movie libraries, 
               because Flick simplifies the process of selecting the perfect movie to watch together.`],
-              [`🎬 Create an Account: Sign up and personalize your profile. Your movie preferences matter!`,
-              `🍿 Join Theatres: Enter or create your own private "Theatre" groups. Invite your friends, family, 
+              [`Create an Account 🎬: Sign up and personalize your profile. Your movie preferences matter!`,
+              `Join Theatres 🍿: Enter or create your own private "Theatre" groups. Invite your friends, family, 
               or colleagues to join your Theatre and embark on cinematic adventures together.`,
-              `👍👎 Like or Dislike: Express your movie preferences by giving movies a thumbs-up or thumbs-down. 
+              `Like or Dislike 👍👎: Express your movie preferences by giving movies a thumbs-up or thumbs-down. 
               Your choices influence the group's decision.`,
-              `📢 Mutual Movie Notifications: Flick's intelligent algorithm detects movies that received unanimous likes within your Theatre. 
+              `Mutual Movie Notifications 📢: Flick's intelligent algorithm detects movies that received unanimous likes within your Theatre. 
               When a movie gets the green light from everyone, 
               you'll be instantly notified! No more back-and-forth messages about movie selections.`],
             ]}
@@ -65,8 +80,21 @@ function Projects({projectsRef}) {
           />
           <Card 
             title={"Flick - Your Ultimate Movie Night Decision-Maker!"}
-            subtitles={["My Role/Tech Used"]}
-            contents={[["content"]]}
+            subtitles={[
+              "Tech Used",
+              "My Role",
+              "My Contributions"
+            ]}
+            contents={[
+              ["Frontend: React.js",
+              "Backend: Express.js, Node.js, PostgreSQL",
+              "Other Tools: Git, Insomnia"],
+              ["Backend Developer"],
+              [`Developed 15 API endpoints using Node.js and Express.js that interacted with 5 tables in PostgreSQL.`,
+              `Wrote efficient database loading and clearing scripts, resulting in a 15% reduction in testing time.`,
+              `Accelerated software implementation by 20% through my role in the planning, analysis, and design phases
+              of the Software Development Life Cycle.`,]
+            ]}
             link={"https://github.com/flick-cp317"}
           />
           <Card 
